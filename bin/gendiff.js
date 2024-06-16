e7d049a0170d3888be202d3c21b748cfae020561
Pleshakov Alexsnder <0542969@mail.ru>
2024-06-16 20:56:47 +0300

    add -f options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,8 @@
 import { program } from "../node_modules/commander/esm.mjs";
 
 program
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
-
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
 program.parse();
