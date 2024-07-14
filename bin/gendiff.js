f5ad9f8678c7ba326d8a7e0f663e91285e3727fe
Pleshakov Alexsnder <0542969@mail.ru>
2024-07-14 19:36:24 +0300

    test4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,9 @@ const program = new Command();
 program
   .arguments('<filePath1> <filePath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
+  .version('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .action((file1, file2, option) => {
-    console.log(gendiff(file1, file2, option.format));
+  .action((file1, file2, { format }) => {
+    console.log(gendiff(file1, file2, format));
   });
 program.parse();
