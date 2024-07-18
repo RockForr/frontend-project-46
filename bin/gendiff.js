277410e6138454a9844d8940e1001e8ae1548844
Pleshakov Alexsnder <0542969@mail.ru>
2024-07-18 08:07:36 +0300

    fix3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { program } from 'commander';
 import gendiff from '../src/gendiffSrc.js';
-
-const program = new Command();
 
 program
   .arguments('<filePath1> <filePath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .action((file1, file2, { format }) => {
-    console.log(gendiff(file1, file2, format));
+  .action((file1, file2, options) => {
+    console.log(gendiff(file1, file2, options.format));
   });
-program.parse();
+program.parse(process.argv);
